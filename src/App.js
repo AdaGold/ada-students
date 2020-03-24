13e59171cb2ea16f26e5278b9c48f2915c0613f9
CheezItMan <chris@adadevelopersacademy.org>
2020-03-23 18:32:40 -0700

    replace Student with StudentCollection in App.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Student from './components/Student';
+import StudentCollection from './components/StudentCollection';
 import './App.css';
 
 function App () {
   return (
     <div className="App">
-      <Student
-        fullName="Improved Ada"
-        email="improved-ada@ada.org"
-      />
+      <StudentCollection />
     </div>
   );
 }
