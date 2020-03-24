da22e441084067f8c4cde8877b70a99c385814c9
CheezItMan <chris@adadevelopersacademy.org>
2020-03-23 18:40:29 -0700

    App component supplies the list of students

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import Student from './components/Student';
 import StudentCollection from './components/StudentCollection';
 import './App.css';
 
+const students = [
+  {
+    fullName: "Ada Lovelace",
+    email: "ada@lovelace.uk",
+  },
+  {
+    fullName: "Katherine Johnson",
+    email: "kat@nasa.gov",
+  },
+];
+
 function App () {
   return (
     <div className="App">
-      <StudentCollection />
+      <StudentCollection students={students} />
     </div>
   );
 }
