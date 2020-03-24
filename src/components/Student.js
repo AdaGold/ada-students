af1634c5b92d0a64ab948e7193bea43557f625bc
CheezItMan <chris@adadevelopersacademy.org>
2020-03-23 18:26:17 -0700

    finished Student hardcoded example

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-const Student = () => {
+const Student = (props) => {
   // Component functions always return JSX
   return (
     <div>
       <h3>Ada Lovelace</h3>
-      <p>
-        Class: C13
-        Birthday: December 10th, 1815
-      </p>
+      <ul>
+        <li>Class: C13</li>
+        <li>Birthday: December 10th, 1815</li>
+        <li>Email: ada@adadev.org</li>
+      </ul>
     </div>
   );
 };
