d5bd5ad19b1288f50e0848611e0c8e9d296d4644
CheezItMan <chris@adadevelopersacademy.org>
2020-03-23 17:37:29 -0700

    hardcode some data

--- src/components/Student.js
+++ src/components/Student.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Student = () => {
   // Component functions always return JSX
   return (
-    <h3>Student Component</h3>
+    <h3>Ada Lovelace</h3>
+    <p>
+      Class: C13
+      Birthday: December 10th, 1815
+    </p>
   );
 };
 
