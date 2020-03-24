baef08fe00d27b75c51dbd25078069f03f7f1656
CheezItMan <chris@adadevelopersacademy.org>
2020-03-24 15:57:46 -0700

    making App Stateful

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Student from './components/Student';
 import StudentCollection from './components/StudentCollection';
 import './App.css';
 
 const students = [
   {
+    id: 1,
     fullName: "Ada Lovelace",
     email: "ada@lovelace.uk",
     present: true,
   },
   {
+    id: 2,
     fullName: "Katherine Johnson",
     email: "kat@nasa.gov",
     present: false,
@@ -17,9 +19,26 @@ const students = [
 ];
 
 function App () {
+  console.log('rendering');
+  const [studentList, setStudentList] = useState(students);
+
+  const updateStudent = (updatedStudent) => {
+    const students = [];
+
+    studentList.forEach((student) => {
+      if (student.id === updatedStudent.id) {
+        students.push(updatedStudent);
+      } else {
+        students.push(student);
+      }
+    });
+
+    setStudentList(students);
+  }
+
   return (
     <div className="App">
-      <StudentCollection students={students} />
+      <StudentCollection students={studentList} onUpdateStudent={updateStudent} />
     </div>
   );
 }
