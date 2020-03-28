42586b6f93a86536cff315f1730e8707ae3d0146
CheezItMan <chris@adadevelopersacademy.org>
2020-03-27 23:22:15 -0700

    add event handler functions

--- src/components/NewStudentForm.js
+++ src/components/NewStudentForm.js
@@ -3,6 +3,38 @@ import './NewStudentForm.css';
 
 
 const NewStudentForm = (props) => {
+  const [formFields, setFormFields] = useState({
+    fullName: '',
+    email: '',
+  });
+
+  // event handlers
+  const onNameChange = (event) => {
+    console.log(`Name Field updated ${ event.target.value }`);
+    setFormFields({
+      fullName: event.target.value,
+      email: formFields.email,
+    });
+  };
+
+  const onEmailChange = (event) => {
+    console.log(`Email Field updated ${ event.target.value }`);
+    setFormFields({
+      fullName: formFields.fullName,
+      email: event.target.value,
+    });
+  }
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    props.addStudentCallback(formFields);
+
+    setFormFields({
+      fullName: '',
+      email: '',
+    });
+  };
 
   return (
     <form className="new-student-form">
