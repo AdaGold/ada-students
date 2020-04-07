82ef6b988977c045bc80cc3c2920871da40aac11
CheezItMan <chris@adadevelopersacademy.org>
2020-04-07 11:15:09 -0700

    import useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import StudentCollection from './components/StudentCollection';
 import NewStudentForm from './components/NewStudentForm';
 import './App.css';
